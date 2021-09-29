694bc57d44681981d5299315eb0db02d762b8cea
Chiranjeev <6258860@gmail.com>
2021-09-29 11:36:30 +0530

    folder structure improved

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 // Importing Contexts for Authentication
 import AuthContextProvider from './contexts/AuthContextProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<AuthContextProvider>
-			<App />
-		</AuthContextProvider>
+		<BrowserRouter>
+			<AuthContextProvider>
+				<App />
+			</AuthContextProvider>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
