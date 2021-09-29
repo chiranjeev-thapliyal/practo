fcba01edf7af6c629579b28d48160992eacba948
Soumya <soumyabb2015@gmail.com>
2021-09-29 15:19:57 +0530

    made some changes in find doctors page

--- src/components/FindDoctors/FindDoctors.jsx
+++ src/components/FindDoctors/FindDoctors.jsx
@@ -25,6 +25,48 @@ export default function FindDoctors() {
             </a>
           </div>
         </div>
+        <div class="c-icon-list">
+          <div class="c-icon-list__list_wrapper">
+            <a
+              class="c-icon"
+              href="/consult?product=fabric&amp;attribution=Home_Banner_Footer"
+            >
+              <i class="tab-icon c-icon__img icon-ic_chat"></i>
+              <span>Consult with a doctor</span>
+            </a>
+            <a class="c-icon" href="/order?utm_source=practo_home">
+              <i class="tab-icon c-icon__img icon-ic_cart"></i>
+              <span>Order Medicines</span>
+            </a>
+            <a
+              class="c-icon"
+              href="https://drive.practo.com?utm_source=practo_home"
+            >
+              <i class="tab-icon c-icon__img icon-ic_records_3"></i>
+              <span>View medical records</span>
+            </a>
+            <a class="c-icon" href="/tests?utm_source=practo_home">
+              <div class="tab-offer">
+                <i class="tab-icon c-icon__img icon-ic_lab_home"></i>
+                <span class="c-offer-tag u-round-corner u-border-white u-t-bold u-t-white">
+                  New
+                </span>
+              </div>
+              <span>Book test</span>
+            </a>
+            <a class="c-icon" href="/healthfeed?utm_source=practo_home">
+              <i class="tab-icon c-icon__img icon-ic_read"></i>
+              <span>Read articles</span>
+            </a>
+            <a class="c-icon" href="/providers?utm_source=practo_home">
+              <i class="tab-icon c-icon__img icon-ic_briefcase"></i>
+              <span>For healthcare providers</span>
+            </a>
+          </div>
+        </div>
+        <div className={styles.secondPage}>
+          <div></div>
+        </div>
       </div>
     </>
   );
