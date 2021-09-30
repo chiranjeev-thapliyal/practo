fa855a18e0405f0f15aa8666fe727bdda0fd6ce3
Pratik <sahap187@gmail.com>
2021-09-30 21:21:54 +0530

    srearch bar design fixed

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ import CardsWrapper from "./components/Main/Cards/CardsWrapper";
 import DownloadWrapper from "./components/Main/Download/DownloadWrapper";
 import Footerwrapper from "./components/Footer/Footerwrapper";
 import FindDoctors from "./components/FindDoctors/FindDoctors";
-import Navbar from "./components/Navbar/Navbarwrapper";
+
 import Consultdoctor from "./components/Main/Consultdoctor/Consultdoctor";
-import SearchWrapper from "./components/SearchResults/SearchWrapper";
+import Navbarwrapper from "./components/Navbar/Navbarwrapper";
+import Searchbar from "./components/SearchBar/Searchbar";
+import Slider from "./components/Slider/Slider";
+
 
 function App() {
   return (
     <div className="App">
-		<Navbar/>
+		<Navbarwrapper/>
       <Switch>
         <Route exact path="/">
           <Main>
+			  <Searchbar/>
             <Banner bannerImg="https://www.practostatic.com/consumer-home/desktop/images/1597423628/practo_care_dweb_banner.png" />
             <CardsWrapper />
 			      <Consultdoctor/>
+			       <Slider/>
+      
             <DownloadWrapper />
           </Main>
         </Route>
@@ -27,7 +33,6 @@ function App() {
           <FindDoctors />
         </Route>
         <Route path="/search">
-          <SearchWrapper />
         </Route>
       </Switch>
       <Footerwrapper />
