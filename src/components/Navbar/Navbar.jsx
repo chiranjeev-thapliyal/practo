ff15422482502a5cd7467d2c992f04a4230bc51d
Chiranjeev <6258860@gmail.com>
2021-10-01 15:13:33 +0530

    navbar ui improved, db.json rectified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,85 +1,82 @@
-  import styled from "styled-components";
+import styled from 'styled-components';
 
-  const Navbar= styled.div`
-  
-  font-weight: 400;
-  border-bottom: 1px solid #f0f0f5;
-  background-color: #fafafa;
-  -webkit-tap-highlight-color: transparent;
-  user-select: none;
-  width: 100%;
-    z-index: 2000;
-    top: 0;
-    
-    
-    & > .nav{
-    margin:0px;
-    background-color: #fff;
-    height:74px
-    padding: 0;
-    color: #414146;
-    box-sizing: border-box;
-    border-bottom: 1px solid #f0f0f5;
-    border-radius: 0;
-    display: block !important;
-    
+const Navbar = styled.div`
+	font-weight: 400;
+	border-bottom: 1px solid #f0f0f5;
+	background-color: #fafafa;
+	-webkit-tap-highlight-color: transparent;
+	user-select: none;
+	width: 100%;
+	z-index: 2000;
+	top: 0;
+	position: ${({ fixed }) => {
+		return fixed ? 'fixed' : 'relative';
+	}}; 
 
-      & > .wrapper{
-       
-        cursor: auto;
-        display: block;
-        width: 100%;
-        height: 100%;
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 0 50px;
-        
-      
+	& > .nav {
+		margin: 0px;
+		background-color: #fff;
+		height: 74px;
+		padding: 0;
+		color: #414146;
+		box-sizing: border-box;
+		border-bottom: 1px solid #f0f0f5;
+		border-radius: 0;
+		display: block !important;
 
-      & > .nav-left{
-        vertical-align: middle;
-        width: 158px;
-        box-sizing: border-box;
-        padding: 24px 0 23px;
-        display: inline-block;
-         height: 100%;
-       
-      }
+		& > .wrapper {
+			cursor: auto;
+			display: block;
+			width: 100%;
+			height: 100%;
+			max-width: 1280px;
+			margin: 0 auto;
+			padding: 0 50px;
 
-      & > .nav-mid{
-       
-        height: 100%;
-        vertical-align: top;
-        display: inline-block;
-        box-sizing: border-box;
-        
-        & > .prod{
-            display: inline-block;
-            min-width: 112px;
-            margin-right: 20px;
-            padding: 22px 0 9px;
-        
+			& > .nav-left {
+				vertical-align: middle;
+				width: 158px;
+				box-sizing: border-box;
+				padding: 24px 0 23px;
+				display: inline-block;
+				height: 100%;
+			}
 
-          & > .prod-title{
-            font-size: 16px;
-            line-height: 20px;
-            font-weight: bold;
-            color: #2d2d32;
-            letter-spacing: -0.3px;
-          }
+			& > .nav-mid {
+				height: 100%;
+				vertical-align: top;
+				display: inline-block;
+				box-sizing: border-box;
 
-          & > .prod-sub{
-            font-size: 12px;
-    line-height: 18px;
-    color: #787887;
-    letter-spacing: -0.1px;
-          }
-        }
-         }
+				& > .active {
+					border-bottom: 5px solid #14bef0;
+				}
 
-        }
-    }
-  `
+				& > .prod {
+					display: inline-block;
+					min-width: 112px;
+					margin-right: 20px;
+					padding: 22px 0 9px;
+					text-decoration: none;
 
+					& > .prod-title {
+						font-size: 16px;
+						line-height: 20px;
+						font-weight: bold;
+						color: #2d2d32;
+						letter-spacing: -0.3px;
+					}
 
-  export default Navbar
+					& > .prod-sub {
+						font-size: 12px;
+						line-height: 18px;
+						color: #787887;
+						letter-spacing: -0.1px;
+					}
+				}
+			}
+		}
+	}
+`;
+
+export default Navbar;
